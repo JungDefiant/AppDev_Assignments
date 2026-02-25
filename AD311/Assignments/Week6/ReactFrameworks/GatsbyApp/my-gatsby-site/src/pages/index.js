14760c616a0d060148416ec44461c982e308c208
BHabibMeetKai <bade.habib@meetkai.com>
2026-02-24 20:39:19 -0800

    Modified index pages for walkthrough

--- AD311/Assignments/Week6/ReactFrameworks/GatsbyApp/my-gatsby-site/src/pages/index.js
+++ AD311/Assignments/Week6/ReactFrameworks/GatsbyApp/my-gatsby-site/src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        HELLO
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
