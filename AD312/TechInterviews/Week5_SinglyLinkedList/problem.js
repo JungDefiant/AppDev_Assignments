990b6272d4af0a1f466c5c84a33d6c53969f099c
BHabibMeetKai <bjhabib93@gmail.com>
2026-05-12 21:12:17 -0700

    Wrote tests for crud-query-app blog-remix-app and recipe-router-app, completed week 5 tech interview

--- AD312/TechInterviews/Week5_SinglyLinkedList/problem.js
+++ AD312/TechInterviews/Week5_SinglyLinkedList/problem.js
@@ -1,5 +1,61 @@
-function problem(a, b) {
-  return a + b;
+class LinkedListNode {
+	constructor(metric, next) {
+		this.metric = metric;
+		this.next = next;
+	}
 }
 
-module.exports = problem;
+function isHealthRecordSymmetric(root) {
+	if (!root) {
+		return false;
+	}
+
+	if (root.next === null) {
+		return true;
+	}
+
+	let nextNode = root;
+	let seekNode = root.next;
+	let length = 1;
+	while (seekNode !== null) {
+		if (seekNode.next !== null) {
+			length++;
+		} else if (nextNode.metric != seekNode.metric) {
+			return false;
+		}
+
+		seekNode = seekNode.next;
+	}
+
+	let i = 1;
+	nextNode = root.next;
+	seekNode = nextNode.next;
+	while (nextNode !== null) {
+		let j = length - i - 1;
+		if (j < i) {
+			break;
+		}
+		while (seekNode !== null) {
+			if (j == i) {
+				if (nextNode.metric !== seekNode.metric) {
+					return false;
+				}
+
+				break;
+			}
+
+			seekNode = seekNode.next;
+			j--;
+		}
+		i++;
+		nextNode = nextNode.next;
+		seekNode = nextNode.next;
+	}
+
+	return true;
+}
+
+module.exports = {
+	problem: isHealthRecordSymmetric,
+	node: LinkedListNode,
+};
--- AD312/TechInterviews/Week5_SinglyLinkedList/problem.test.js
+++ AD312/TechInterviews/Week5_SinglyLinkedList/problem.test.js
@@ -1,4 +1,44 @@
-const problem = require('./problem');
-test('adds 1 + 2 to equal 3', () => {
-     expect(problem(1, 2)).toBe(3);
+const { problem, node } = require("./problem");
+
+// Standard Tests
+test("test 1", () => {
+	const list = new node(
+		2,
+		new node(3, new node(1, new node(3, new node(2, null)))),
+	);
+	expect(problem(list)).toBe(true);
+});
+
+test("test 2", () => {
+	const list = new node(
+		2,
+		new node(3, new node(1, new node(1, new node(3, new node(2, null))))),
+	);
+	expect(problem(list)).toBe(true);
+});
+
+test("test 3", () => {
+	const list = new node(
+		11,
+		new node(12, new node(13, new node(12, new node(12, new node(11, null))))),
+	);
+	expect(problem(list)).toBe(false);
+});
+
+// Edge Cases
+test("edge case: all the same number", () => {
+	const list = new node(
+		2,
+		new node(2, new node(2, new node(2, new node(2, new node(2, null))))),
+	);
+	expect(problem(list)).toBe(true);
+});
+
+test("edge case: empty node", () => {
+	const list = new node(2, null);
+	expect(problem(list)).toBe(true);
+});
+
+test("edge case: null param", () => {
+	expect(problem(null)).toBe(false);
 });
