06ae218e2cdbe33e9bb6725d877e0116376e3ad8
BHabibMeetKai <bjhabib93@gmail.com>
2026-06-06 15:03:57 -0700

    feat: Implemented Bubble Sort

--- AD312/TechInterviews/Week9_BubbleSort/problem.js
+++ AD312/TechInterviews/Week9_BubbleSort/problem.js
@@ -1,24 +1,48 @@
 function bubbleSort(a) {
-	let sorted = false;
-	let pass = 1;
-	while (!sorted) {
-		let swap = false;
-		let temp = a[0];
-		for (let i = 1; i < a.length - 1; i++) {
-			if (a[i + 1] <= temp) {
+	// Runs for the entire array
+	for (let i = 0; i < a.length; i++) {
+		// Each pass runs from the start of the array to the index
+		// before the sorted part of the array
+		for (let j = 0; j < a.length - 1; j++) {
+			// If next index has a value less than current index,
+			// then swap the values of both indices.
+			if (a[j + 1] < a[j]) {
 				swap = true;
-				a[i] = a[i + 1];
-				a[i + 1] = temp;
+				const temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
-
-			temp = a[i + 1];
 		}
-
-		sorted = !swap;
-
-		console.log(`PASS ${pass}`, a);
-		pass += 1;
 	}
 }
 
-module.exports = selectionSort;
+function optimizedBubbleSort(a) {
+	let sorted = false;
+	let passes = 0;
+
+	// Runs until there is no swap in a pass
+	while (!sorted) {
+		let swap = false;
+		// Each pass runs from the start of the array to the index
+		// before the sorted part of the array
+		for (let i = 0; i < a.length - passes - 1; i++) {
+			// If next index has a value less than current index,
+			// then swap the values of both indices.
+			if (a[i + 1] < a[i]) {
+				swap = true;
+				const temp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = temp;
+			}
+		}
+
+		// Sorted is false if a swap occurred in this pass
+		sorted = !swap;
+
+		// Increment number of passes, indicating the number
+		// of sorted values in the array
+		passes++;
+	}
+}
+
+module.exports = { problem: bubbleSort, optProblem: optimizedBubbleSort };
--- AD312/TechInterviews/Week9_BubbleSort/problem.test.js
+++ AD312/TechInterviews/Week9_BubbleSort/problem.test.js
@@ -1,56 +1,114 @@
 const { randomInt } = require("crypto");
-const problem = require("./problem");
+const { problem, optProblem } = require("./problem");
 
-test("random ints", () => {
-	const randArr = Array.from({ length: 4 }, () => randomInt(10));
-	problem(randArr);
+describe("Original Bubble Sort", () => {
+	test("random ints", () => {
+		const randArr = Array.from({ length: 4 }, () => randomInt(10));
+		problem(randArr);
 
-	expect(randArr[0] <= randArr[1]).toBe(true);
-	expect(randArr[1] <= randArr[2]).toBe(true);
-	expect(randArr[2] <= randArr[3]).toBe(true);
+		expect(randArr[0] <= randArr[1]).toBe(true);
+		expect(randArr[1] <= randArr[2]).toBe(true);
+		expect(randArr[2] <= randArr[3]).toBe(true);
+	});
+
+	test("sorted array, ascending", () => {
+		const arr = [1, 2, 3, 3, 4, 5];
+		problem(arr);
+
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+		expect(arr[4] <= arr[5]).toBe(true);
+	});
+
+	test("sorted array, descending", () => {
+		const arr = [5, 4, 3, 2, 1];
+		problem(arr);
+
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+	});
+
+	test("array of same numbers", () => {
+		const arr = [2, 2, 2, 2, 2];
+		problem(arr);
+
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+	});
+
+	test("empty array", () => {
+		const arr = [];
+		problem(arr);
+
+		expect(!arr[0]).toBe(true);
+	});
+
+	test("array of 1 number", () => {
+		const arr = [2];
+		problem(arr);
+
+		expect(arr[0]).toEqual(2);
+	});
 });
 
-test("sorted array, ascending", () => {
-	const arr = [1, 2, 3, 3, 4, 5];
-	problem(arr);
+describe("Optimized Bubble Sort", () => {
+	test("random ints", () => {
+		const randArr = Array.from({ length: 4 }, () => randomInt(10));
+		optProblem(randArr);
 
-	expect(arr[0] <= arr[1]).toBe(true);
-	expect(arr[1] <= arr[2]).toBe(true);
-	expect(arr[2] <= arr[3]).toBe(true);
-	expect(arr[3] <= arr[4]).toBe(true);
-	expect(arr[4] <= arr[5]).toBe(true);
-});
+		expect(randArr[0] <= randArr[1]).toBe(true);
+		expect(randArr[1] <= randArr[2]).toBe(true);
+		expect(randArr[2] <= randArr[3]).toBe(true);
+	});
 
-test("sorted array, descending", () => {
-	const arr = [5, 4, 3, 2, 1];
-	problem(arr);
+	test("sorted array, ascending", () => {
+		const arr = [1, 2, 3, 3, 4, 5];
+		optProblem(arr);
 
-	expect(arr[0] <= arr[1]).toBe(true);
-	expect(arr[1] <= arr[2]).toBe(true);
-	expect(arr[2] <= arr[3]).toBe(true);
-	expect(arr[3] <= arr[4]).toBe(true);
-});
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+		expect(arr[4] <= arr[5]).toBe(true);
+	});
 
-test("array of same numbers", () => {
-	const arr = [2, 2, 2, 2, 2];
-	problem(arr);
+	test("sorted array, descending", () => {
+		const arr = [5, 4, 3, 2, 1];
+		optProblem(arr);
 
-	expect(arr[0] <= arr[1]).toBe(true);
-	expect(arr[1] <= arr[2]).toBe(true);
-	expect(arr[2] <= arr[3]).toBe(true);
-	expect(arr[3] <= arr[4]).toBe(true);
-});
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+	});
 
-test("empty array", () => {
-	const arr = [];
-	problem(arr);
+	test("array of same numbers", () => {
+		const arr = [2, 2, 2, 2, 2];
+		optProblem(arr);
 
-	expect(!arr[0]).toBe(true);
-});
+		expect(arr[0] <= arr[1]).toBe(true);
+		expect(arr[1] <= arr[2]).toBe(true);
+		expect(arr[2] <= arr[3]).toBe(true);
+		expect(arr[3] <= arr[4]).toBe(true);
+	});
 
-test("array of 1 number", () => {
-	const arr = [2];
-	problem(arr);
+	test("empty array", () => {
+		const arr = [];
+		optProblem(arr);
 
-	expect(arr[0]).toEqual(2);
+		expect(!arr[0]).toBe(true);
+	});
+
+	test("array of 1 number", () => {
+		const arr = [2];
+		optProblem(arr);
+
+		expect(arr[0]).toEqual(2);
+	});
 });
